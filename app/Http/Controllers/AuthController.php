78e46a1cc313d435c065bb8ecabd321dcab624f2
saharoki <80281778+saharoki@users.noreply.github.com>
2021-12-22 01:20:28 +0200

    auth controller

--- app/Http/Controllers/AuthController.php
+++ app/Http/Controllers/AuthController.php
@@ -19,7 +19,7 @@ class AuthController extends Controller
         $this->validate($request, [
             'name' => 'required|string',
             'email' => 'required|email|unique:users',
-            'password' => 'required|confirmed',
+            'password' => 'required',
         ]);
 
         try {
