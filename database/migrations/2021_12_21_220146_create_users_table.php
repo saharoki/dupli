7e4b0e81c27f274356ca780f72effd601da69939
saharoki <80281778+saharoki@users.noreply.github.com>
2021-12-22 00:17:57 +0200

    db user migration

--- database/migrations/2021_12_21_220146_create_users_table.php
+++ database/migrations/2021_12_21_220146_create_users_table.php
@@ -15,8 +15,8 @@ class CreateUsersTable extends Migration
     {
         Schema::create('users', function (Blueprint $table) {
             $table->bigIncrements('id');
-            $table->string('name');
-            $table->string('email')->unique()->notNullable();
+            $table->string('name', 50);
+            $table->string('email', 50)->unique()->notNullable();
             $table->string('password');
             $table->timestamps();
         });
