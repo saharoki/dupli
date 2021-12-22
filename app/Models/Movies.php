a8b3275c4fd95ce0de2760a6e15c85873f7d6d68
saharoki <80281778+saharoki@users.noreply.github.com>
2021-12-22 17:00:44 +0200

    main controller

--- app/Models/Movies.php
+++ app/Models/Movies.php
@@ -15,6 +15,7 @@ class Movies extends Model
     {
         $user = auth()->user();
         return $this->hasOne(Rent::class, 'movie_id', 'id')
-            ->where('user_id', $user->id);
+            ->where('user_id', 1);
+           // ->where('user_id', $user->id);
     }
 }
