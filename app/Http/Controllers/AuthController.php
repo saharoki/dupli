77b184c0a389f54195f1a479d7f129ecf9fd6912
saharoki <80281778+saharoki@users.noreply.github.com>
2021-12-22 16:08:11 +0200

    main controller

--- app/Http/Controllers/AuthController.php
+++ app/Http/Controllers/AuthController.php
@@ -68,4 +68,27 @@ class AuthController extends Controller
     }
 
 
+    /**
+     * Log the user out (Invalidate the token).
+     *
+     * @return \Illuminate\Http\JsonResponse
+     */
+    public function logout()
+    {
+        auth()->logout();
+
+        return response()->json(['message' => 'Successfully logged out']);
+    }
+
+    /**
+     * Refresh a token.
+     *
+     * @return \Illuminate\Http\JsonResponse
+     */
+    public function refresh()
+    {
+        return $this->respondWithToken(auth()->refresh());
+    }
+
+
 }
--- database/migrations/2021_12_22_125528_create_movies_table.php
+++ database/migrations/2021_12_22_125528_create_movies_table.php
@@ -14,7 +14,21 @@ class CreateMoviesTable extends Migration
     public function up()
     {
         Schema::create('movies', function (Blueprint $table) {
-            $table->id();
+            $table->bigIncrements('id');
+            $table->string('name')->notNullable();
+            $table->date('release_date')->notNullable();
+            $table->text('overview')->notNullable();
+            $table->binary('image')->notNullable();
+            $table->float('score', 3, 1)->notNullable();
+            $table->timestamps();
+        });
+
+        Schema::create('rents', function (Blueprint $table) {
+            $table->bigIncrements('id');
+            $table->bigInteger('user_id')->notNullable();
+            $table->bigInteger('movie_id')->notNullable();
+            $table->dateTime('rent_start')->notNullable();
+            $table->dateTime('rent_end')->notNullable();
             $table->timestamps();
         });
     }
@@ -27,5 +41,6 @@ class CreateMoviesTable extends Migration
     public function down()
     {
         Schema::dropIfExists('movies');
+        Schema::dropIfExists('rents');
     }
 }
